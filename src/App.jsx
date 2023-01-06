e9f34d84650f55c824366298b88b268f43eef470
Shaharyar07 <shaharyar.tariq248@gmail.com>
2023-01-06 23:24:47 +0500

    layout changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import Navbar from "./component/Navbar";
 import Hero from "./component/Hero";
 
+import image3 from "./assets/hero.png";
+import About from "./component/About";
 function App() {
   return (
-    <main className=' max-w-[1268px] m-auto'>
-      <Navbar />
-      <Hero />
+    <main>
+      <section
+        style={{
+          backgroundImage: `url(${image3})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          width: "100%",
+          overflow: "hidden",
+          height: "100vh",
+        }}
+      >
+        <div className='m-auto max-w-[1268px]'>
+          <Navbar />
+          <Hero />
+        </div>
+      </section>
+      <About />
     </main>
   );
 }
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,22 +1,12 @@
 import React from "react";
-import heroimg from "../assets/heroimg.png";
+
 const Hero = () => {
   return (
-    <div className='hero min-h-screen '>
-      <div className='hero-content flex-col lg:flex-row-reverse'>
-        <div>
-          <h1 className='text-5xl font-bold'>Box Office News!</h1>
-          <p className='py-6'>
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className='inline-block rounded-lg px-5 py-3 text-sm font-semibold leading-6  shadow-sm ring-1 ring-white/30 hover:ring-white/50'>
-            Get Started
-          </button>
-        </div>
-        <img src={heroimg} className=' opacity-90' />
-      </div>
+    <div className='float-right mt-[150px] w-1/3 '>
+      <h1 className='text-6xl leading font-bold'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
+        corporis?
+      </h1>
     </div>
   );
 };
